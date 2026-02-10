bb05d2df1a1fcd578d39825ef2aa7828df0555b7
stefanvill <Stefan@villegas.se>
2026-02-10 10:21:13 +0100

    Initial commit

---
+++ src/main/java/se/iths/stefan/labbdrift/LabbDriftApplication.java
@@ -0,0 +1,13 @@
+package se.iths.stefan.labbdrift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabbDriftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabbDriftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/stefan/labbdrift/LabbDriftApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.stefan.labbdrift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabbDriftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
