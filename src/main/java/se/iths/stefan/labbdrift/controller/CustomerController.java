0fbfed13d6689a88eefc36ab575306d591bb5695
Joajak92 <Joakim.jakobsson@iths.segit config --global user.email Joakim.jakobsson@iths.se--global user.name Joajak92--global user.email Joakim.jakobsson@iths.se>
2026-02-12 12:59:15 +0100

    Skapade CustomerController med tillhörande html filer samt en Dockerfile

---
+++ src/main/java/se/iths/stefan/labbdrift/controller/CustomerController.java
@@ -0,0 +1,59 @@
+package se.iths.stefan.labbdrift.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import se.iths.stefan.labbdrift.model.Customer;
+import se.iths.stefan.labbdrift.service.CustomerService;
+
+@Controller
+@RequestMapping("/customers")
+public class CustomerController {
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping
+    public String getAllCustomers(Model model) {
+        model.addAttribute("customers", customerService.getAllCustomers());
+        return "customers";
+    }
+
+    @GetMapping("/new")
+    public String showCreateForm() {
+        return "create-customer";
+    }
+
+    @PostMapping
+    public String createCustomer(@ModelAttribute Customer customer) {
+        Customer customer2 = customerService.createCustomer(customer);
+        return "redirect:/customers";
+    }
+
+    @GetMapping("/{id}")
+    public String findCustomerById(@PathVariable Long id, Model model) {
+        model.addAttribute("customers", customerService.findCustomer(id));
+        return "customers";
+    }
+
+    @PutMapping("/{id}")
+    public String updateCustomer(@PathVariable Long id, @ModelAttribute Customer customer) {
+        customerService.updateCustomer(id, customer);
+        return "redirect:/customers";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String showUpdateForm(@PathVariable Long id, Model model) {
+        Customer customer = customerService.findCustomer(id);
+        model.addAttribute("customer", customer);
+        return "edit-customer";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteCustomer(@PathVariable Long id) {
+        customerService.deleteCustomer(id);
+        return "redirect:/customers";
+    }
+}
